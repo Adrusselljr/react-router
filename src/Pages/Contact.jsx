16b1200b9492e23d54c561b63b52025910a5de5b
Aaron Russell <cpl.adrusselljr@gmail.com>
2022-04-12 12:12:59 -0400

    modified App.css/Contact.jsx/Form.jsx/index.js

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -7,9 +7,10 @@ function Contact() {
     return (
         <div className='App'>
 
-<            div className="home">
+            <div className="home">
                 <h1>Contact Page</h1><br/>
                 <Link to="form" className='btn btn-primary'>Create User</Link>
+                {/* <Link to={`form?name=${nameSearchParam}&phone=${phoneSearchParam}&age=${ageSearchParam}$email=${emailSearchParam}`} className='btn btn-primary'>Share</Link> */}
                 <Outlet/>
             </div>
 
--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Link, useSearchParams } from 'react-router-dom'
 import '../App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 function Form(props) {
+    const [click, setClick] = useState(false)
+    const [searchParams] = useSearchParams()
+    const nameSearchParam = searchParams.get("name")
+    const phoneSearchParam = searchParams.get("phone")
+    const ageSearchParam = searchParams.get("age")
+    const emailSearchParam = searchParams.get("email")
+    
+    useEffect(() => {
+        props.setName(nameSearchParam)
+        props.setPhone(phoneSearchParam)
+        props.setAge(ageSearchParam)
+        props.setEmail(emailSearchParam)
+    }, [nameSearchParam, phoneSearchParam, ageSearchParam, emailSearchParam])
+
+    const handleOnClick = () => {
+        setClick(prevState => !prevState)
+    }
 
     return (
         <div className='App'>
@@ -12,26 +29,34 @@ function Form(props) {
             <div className="form">
                 <div className="form-group">
                     <label>Name</label>
-                    <input onChange={ e => props.setName(e.target.value) } type="text" className='form-control'/>
+                    <input value={ props.name } onChange={ e => props.setName(e.target.value) } type="text" className='form-control'/>
                 </div>
 
                 <div className="form-group">
                     <label>Phone</label>
-                    <input onChange={ e => props.setPhone(e.target.value) } type="tel" className='form-control'/>
+                    <input value={ props.phone } onChange={ e => props.setPhone(e.target.value) } type="tel" className='form-control'/>
                 </div>
 
                 <div className="form-group">
                     <label>Age</label>
-                    <input onChange={ e => props.setAge(e.target.value) } type="number" className='form-control'/>
+                    <input value={ props.age } onChange={ e => props.setAge(e.target.value) } type="number" className='form-control'/>
                 </div>
 
                 <div className="form-group">
                     <label>Email</label>
-                    <input onChange={ e => props.setEmail(e.target.value) } type="email" className='form-control'/>
+                    <input value={ props.email } onChange={ e => props.setEmail(e.target.value) } type="email" className='form-control'/>
                 </div><br/>
 
-                <Link to="/contact/submitted" className='btn btn-primary'>Submit</Link>
+                <div className="links">
+                    <Link to="/contact/submitted" className='btn btn-primary'>Submit</Link>
+                    <button onClick={ handleOnClick } className='btn btn-primary'>Generate URL</button><br/><br/>
+                </div>
             </div>
+
+            {click === true
+                ? <p className='url'>{ `http://localhost:3000/contact/form?name=${props.name}&phone=${props.phone}&age=${props.age}$email=${props.email}` }</p>
+                : ""
+            }
         
         </div>
     )
